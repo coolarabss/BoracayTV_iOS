eb6ba663345ecb37a6ce0432f4faac3a2d323f4d
euisuk_lee <coolarabs@zigbang.com>
2018-03-22 11:20:06 +0900

    add webview

--- BoracayTV/ViewController.swift
+++ BoracayTV/ViewController.swift
@@ -7,19 +7,37 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    var webView : WKWebView!
+    
+    let basicUrl = URLRequest(url: URL(string: "https://boracaytv.in/")!)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        UIApplication.shared.statusBarView?.backgroundColor = UIColor(red: 0, green: 0.588, blue: 0.529, alpha: 1)
+        
+        //init webbiew
+        webView = WKWebView(frame: self.view.frame)
+        webView.scrollView.showsVerticalScrollIndicator = false
+        webView.allowsBackForwardNavigationGestures = true
+        webView.load(basicUrl)
+        self.view.addSubview(webView)
+        self.view.sendSubview(toBack: webView)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 }
 
